c4423c18fc5f7cc9bfe41aaa4ab2f46bc4f159d0
user <user@MacBook-Air-de-user.local>
2022-02-15 23:07:40 +0100

    initial commit

---
+++ script/data.js
@@ -0,0 +1,44 @@
+const data = {
+    movieFile : [
+        {
+            id: 1,
+            image: "../img/Beru Whitesun lars.jpeg"
+        },
+        {
+            id: 2,
+            image: "../img/Biggs Darklighter.jpeg"
+        },
+        {
+            id: 3,
+            image: "../img/C-3PO.jpeg"
+        },
+        {
+            id: 4,
+            image: "../img/Darth Vader.jpeg"
+        },
+        {
+            id: 5,
+            image: "../img/Leia Organa.jpeg"
+        },
+        {
+            id: 6,
+            image: "../img/Luke Skywalker.jpeg"
+        },
+        {
+            id: 7,
+            image: "../img/Obi-Wan Kenobi.jpeg"
+        },
+        {
+            id: 8,
+            image: "../img/Owen Lars.jpeg"
+        },
+        {
+            id: 9,
+            image: "../img/R2-D2.jpeg"
+        },
+        {
+            id: 10,
+            image: "../img/R5-D4.jpeg"
+        },
+    ]
+}
--- script/index.js
+++ script/index.js
@@ -1,3 +1,49 @@
-function main() {}
+let body = document.querySelector("body");
 
-module.exports = { main }
+const modalDiv = document.querySelector(".modal-div");
+const close_modal = document.querySelector(".close");
+const modal_span = document.querySelector(".modal-span");
+
+const openModal = () => {
+  modalDiv.style.display = "block";
+};
+
+const closeModal = () => {
+  modalDiv.style.display = "none";
+  console.log("In!");
+};
+
+close_modal.addEventListener("click", closeModal);
+body.addEventListener("click", closeModal);
+
+const getMovieCard = document.querySelector(".movieCard");
+
+const { movieFile } = data;
+
+const getIdAndCallApi = async (id) => {
+  // console.log(id)
+  const response = await fetch(`https://swapi.dev/api/people/${id}`);
+  const result = await response.json();
+  console.log(result);
+  if (result) {
+    modal_span.innerHTML = `
+  
+        <ul class="unorderd-list">
+          <li class ="name"> ${result.name}</li>
+          <li class="gender">${result.gender}</li>
+          <li class = "height">${result.height}</li>
+        </ul>
+      
+    `;
+  }
+  if (result) return openModal();
+};
+
+movieFile &&
+  movieFile.map(({ id, image }) => {
+    return (getMovieCard.innerHTML += `
+    <div id="${id}" onclick="getIdAndCallApi(${id})"> 
+   <img src="${image}" class = "imgTag">
+    </div>
+  `);
+  });
